4df9fe43981e9fea390861a03966eaae60f6dd3c
pfory <pfory@seznam.cz>
2016-06-01 15:10:37 +0200

    +

--- configurations/rules_refresh.sh
+++ configurations/rules_refresh.sh
@@ -1,5 +1,5 @@
 # This script is called by openHAB after persistance service was started
-sleep 5
+sleep 20
 cd /opt/openhab/configurations/rules/
 RULES=`find *.rules | grep -v refresh.rules`
 for f in $RULES
